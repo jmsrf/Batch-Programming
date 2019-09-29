092ae3836d1c0c06e03fb2edba4e1daf36dd0c32
jmseppala <jmseppala90@gmail.com>
2019-09-29 15:22:49 +0300

    Github_username.bat

---
+++ Github_username.bat
@@ -0,0 +1,13 @@
+:: This program adds GitHub username provided by user as part of URL and opens browser to that profile.
+:: GitHub profile URLs are currently github.com/profilename 
+@echo off
+:start
+cls
+echo.
+echo Github-profile opener
+echo.
+echo Please type GitHub-username and your Internet browser will open it.
+echo.
+set /p Input=
+start https://www.github.com/%Input%
+goto start

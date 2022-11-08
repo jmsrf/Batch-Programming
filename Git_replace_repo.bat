5aa39a10d61a675a529e9048ae01afec23c99cb3
jmsep <jmseppala90@gmail.com>
2022-11-08 21:05:08 +0200

    Update Git_replace_repo.bat

--- Git_replace_repo.bat
+++ Git_replace_repo.bat
@@ -1,34 +1,47 @@
 REM This Git example works on Windows. Can be modified to work on Linux and also order can be changed to suite needs. 
 REM Basic idea is to provide simple way to delete old test repo locally and clone it again as an empty repo for fresh start.
+
 @echo off
 set PATH=%PATH%;C:\Program Files\Git\cmd
+
 REM Replace next line with correct path to local repository
+
 cd C:\Users\username\Desktop\test
+
 REM Clear remote branch by deleting old files from repository locally then committing and pushing deletions to remote.
+
 del file1
 del file2
 del file3
 git add .
-git commit -m "clear branch"
+git commit -m "Clear branch"
 git push
+
 REM Replace next line with correct path to repository to be cloned
 cd C:\Users\username\Desktop
+
 REM Remove repository folder locally for cloning and replacing it with empty repository for a fresh start to testing Git commands
 RD /S /Q "C:\Users\username\Desktop\test"
+
 REM Replace next line with url to remote repository on GitHub for example
 git clone urltoremoterepository
+
 REM Change directory to cloned repository (repository name here is test). Replace with correct path to local folder.
 cd C:\Users\username\Desktop\test
+
 TIMEOUT /T 4
+
 REM Create and commit three files as separate commits. Timeout added for allowing time to multiple commits in a row.
 type nul >> file1
 git add .
-git commit -m "commit1"
+git commit -m "Commit1"
 TIMEOUT /T 4
+
 type nul >> file2
 git add .
-git commit -m "commit2"
+git commit -m "Commit2"
 TIMEOUT /T 4
+
 type nul >> file3
 git add .
-git commit -m "commit3"
+git commit -m "Commit3"
